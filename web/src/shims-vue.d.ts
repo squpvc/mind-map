448a6104f55b965080eb51d2bda7d6decbfb652b
Eng. Mohammad Abbadi <mohammad.abbadi@outlook.com>
2025-07-15 01:10:04 +0300

    run fixing

---
+++ web/src/shims-vue.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
+
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
